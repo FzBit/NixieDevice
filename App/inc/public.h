4517d3d80592b9d3c296ff48ba8761f66863d7e1
Frumig <Alamu.Frumig@gmail.com>
2020-03-09 22:24:40 +0800

    Update

--- App/inc/public.h
+++ App/inc/public.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: Frumig
+ * @Date: 2020-02-07 13:41:25
+ * @LastEditTime: 2020-03-09 22:17:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \NixieDevice-master\App\inc\public.h
+ */
 
 #ifndef PUBLIC_H
 #define PUBLIC_H
@@ -6,5 +14,10 @@
 #include <tick.h>
 #include <74HC595.h>
 #include <Sth30.h>
+
+#define BEE_ON GPIO_ResetBits(GPIOA, GPIO_Pin_15)
+#define BEE_OFF GPIO_SetBits(GPIOA, GPIO_Pin_15)
+
+void GPIO_Config();
 
 #endif
